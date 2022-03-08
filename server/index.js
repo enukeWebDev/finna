4e10f41b1004b0eb5579c2801385b3b54f9c1c9c
Roderick <enuke4@gmail.com>
2022-03-08 12:55:13 -0600

    create .env file

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 // Entry point of the Application
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -17,14 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// This credential will be moved to .env file before the deployment
-const CONNECTION_URL = 'mongodb://finna4:finna1234@nukecluster-shard-00-00.ws2ks.mongodb.net:27017,nukecluster-shard-00-01.ws2ks.mongodb.net:27017,nukecluster-shard-00-02.ws2ks.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mjthyn-shard-0&authSource=admin&retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen
     (PORT, () => console.log(`Server running on PORT: ${PORT}`)))
 
   .catch((error) => console.log(error.message))
-
-// mongoose.set('useFindAndModify', false);
